9bc0d5de53b00a8aa334d30b6e5cf1cec3caabf1
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-03-09 23:52:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    int a,b;
-    char c;
-    scanf("%d %d %c",&a,&b,&c);
-    switch(c){
-        case'+':
-            printf("%d",a,b,a+b);
+    int num1, num2;
+    char operator;
+    int result;
+
+    // Asking for input
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+    printf("Enter the arithmetic operation (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    // Performing the operation
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("The result is: %d\n", result);
             break;
-        case'-':
-            printf("%d",a-b);
+        case '-':
+            result = num1 - num2;
+            printf("The result is: %d\n", result);
             break;
-        case'*':
-            printf("%d",a*b);
+        case '*':
+            result = num1 * num2;
+            printf("The result is: %d\n", result);
             break;
-        case'/':
-            printf("%d",a/b);
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("The result is: %d\n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
             break;
-        
+        default:
+            printf("Invalid operator. Please use +, -, *, or /.\n");
     }
+
     return 0;
 }
