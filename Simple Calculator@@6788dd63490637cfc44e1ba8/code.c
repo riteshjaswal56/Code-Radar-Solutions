1d6d5669d41406d8cdb02911f541764689148652
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-03-10 23:44:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -22,7 +22,7 @@ int main() {
                 result = a / b;
                 printf("%d",result);
             } else {
-                printf("Error\n");
+                printf("error\n");
             }
             break;
         default:
