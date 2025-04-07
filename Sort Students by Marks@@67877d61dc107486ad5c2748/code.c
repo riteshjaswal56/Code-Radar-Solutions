7889ab6e03f3539f18839eced54ddb19652e08ed
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-07 19:10:46 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -5,12 +5,10 @@ struct Student {
     float marks;
 };
 
-// Function to sort students by marks in descending order
 void sortByMarks(struct Student students[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (students[j].marks < students[j + 1].marks) {
-                // Swap the students
                 struct Student temp = students[j];
                 students[j] = students[j + 1];
                 students[j + 1] = temp;
