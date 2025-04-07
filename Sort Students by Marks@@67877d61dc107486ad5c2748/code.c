5c661d1681c9fea87bed3e42ee17740980429e11
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-07 19:06:20 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-
 struct Student {
     int rollNumber;
     char name[50];
     float marks;
 };
 
-void sortByRollNumber(struct Student students[], int n) {
+// Function to sort students by marks in descending order
+void sortByMarks(struct Student students[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (students[j].rollNumber > students[j + 1].rollNumber) {
+            if (students[j].marks < students[j + 1].marks) {
+                // Swap the students
                 struct Student temp = students[j];
                 students[j] = students[j + 1];
                 students[j + 1] = temp;
@@ -24,14 +24,15 @@ int main() {
     scanf("%d", &n);
 
     struct Student students[n];
+
     for (i = 0; i < n; i++) {
         scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);
     }
 
-    sortByRollNumber(students, n);
+    sortByMarks(students, n);
 
     for (i = 0; i < n; i++) {
-        printf("%.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
+        printf("Roll Number: %d, Name: %s, Marks: %.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
     }
 
     return 0;
