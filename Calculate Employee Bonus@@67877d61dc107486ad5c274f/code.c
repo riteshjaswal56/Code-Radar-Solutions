a1261c59a82c5277aa35a22b789b49df75f5a208
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-08 18:11:36 +0530

    Solution to Calculate Employee Bonus

--- Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -24,7 +24,7 @@ int main() {
 
     }
     for (i = 0; i < n; i++) {
-        printf(" Employee ID: %d,", bonus[i].number);
+        printf("Employee ID: %d,", bonus[i].number);
         printf(" Name: %s,", bonus[i].name);
         printf(" Bonus: %.2f \n", bonus[i].salary);
     }
