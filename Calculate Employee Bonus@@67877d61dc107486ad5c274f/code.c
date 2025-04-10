c8df98547e0bc4bca5064a856407b9882b39abe8
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-10 14:10:03 +0530

    Solution to Calculate Employee Bonus

--- Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -13,34 +13,9 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d %s %f", &bonus[i].number, &bonus[i].name, &bonus[i].salary);
     } 
-    if Here's an example of a C program using a structure to calculate employee bonuses based on their salary:
-#include <stdio.h>
+    if 
 
-// Define a structure for Employee
-struct Employee {
-    char name[50];
-    float salary;
-    float bonus;
-};
-
-int main() {
-    struct Employee emp;
-
-    // Input employee details
-    printf("Enter employee name: ");
-    scanf("%s", emp.name);
-    printf("Enter employee salary: ");
-    scanf("%f", &emp.salary);
-
-    // Calculate bonus
-    if (emp.salary < 50000) {
-        emp.bonus = emp.salary * 0.10;
-    } else {
-        emp.bonus = emp.salary * 0.05;
-    }
-
-
-
+    
     for (i = 0; i < n; i++) {
         printf("Employee ID: %d,", bonus[i].number);
         printf(" Name: %s,", bonus[i].name);
