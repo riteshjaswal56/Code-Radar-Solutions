b4c960548a728e9e81856ea2747fbca7fb74b040
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-10 19:03:40 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -16,15 +16,15 @@ int main(){
 
     for (i=0;i<n;i++){
         scanf("%s %s %f",&service[i].name,&service[i].subscription,&service[i].revenue);
-        if(strcmp(service[i].subscription,"basic")==0){
+        if(strcmp(service[i].subscription,"basic")){
             count_basic++;
             revenue_basic = service[i].revenue;
         }
-        else if(strcmp(service[i].subscription,"standard")==0){
+        else if(strcmp(service[i].subscription,"standard")){
             count_standard++;
             revenue_standard = service[i].revenue;
         }
-        else if(strcmp(service[i].subscription,"premium")==0){
+        else if(strcmp(service[i].subscription,"premium")){
             count_premium++;
             revenue_premium = service[i].revenue;
         }
