f3a9114836faab4b5b12fcbe24205adcc2b3e533
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-10 14:56:19 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-struct management{
-    char number [50];
-    char vehicle [50];
+#include <string.h>
+
+struct Vehicle {
+    char number[50];
+    char type[50];
     int toll;
 };
-int main(){
-    int n,i;
-    scanf("%d",&n);
-    struct management plaza[n];
-    for (i=0 ; i<n ; i++){
-        scanf("%s %s %d",&plaza[i].number,&plaza[i].vehicle,&plaza[i].toll);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Vehicle vehicles[n];
+
+    int carToll = 0, truckToll = 0, bikeToll = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s %s %d", vehicles[i].number, vehicles[i].type, &vehicles[i].toll);
+
+        if (strcmp(vehicles[i].type, "Car") == 0) {
+            carToll += vehicles[i].toll;
+        } else if (strcmp(vehicles[i].type, "Truck") == 0) {
+            truckToll += vehicles[i].toll;
+        } else if (strcmp(vehicles[i].type, "Bike") == 0) {
+            bikeToll += vehicles[i].toll;
+        }
     }
 
-    printf("Cars: %.2f, ",plaza[i].toll);
-    printf("Trucks: %.2f, ",plaza[i].toll);
-    printf("Bikes: %.2f ",plaza[i].toll);
-    
-    return 0;
+    printf("Cars: %d, Trucks: %d, Bikes: %d\n", carToll, truckToll, bikeToll);
 
+    return 0;
 }
