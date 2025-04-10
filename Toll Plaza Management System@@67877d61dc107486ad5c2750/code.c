dee8784c546b454b0e74bb6023963194ddabd80d
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-10 14:40:56 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct management{
+    char number [50];
+    char vehicle [50];
+    int toll;
+};
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    struct management plaza[n];
+    for (i=0 ; i<n ; i++){
+        scnaf("%s %s %d",&plaza[i].number;&plaza[i].vehicle,&plaza[i].toll);
+    }
+    for (i=0 ; i<n ; i++){
+        printf("Cars: %.2f, ",plaza[i].vehicle);
+    }
+    return 0;
+}
