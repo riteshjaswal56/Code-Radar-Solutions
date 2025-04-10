1f56cae6e707a21e8b0d006e9f5cb4da0cb3f85f
riteshjaswal56 <riteshjaswal56@gmail.com>
2025-04-10 14:32:57 +0530

    Solution to Calculate Employee Bonus

--- Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -14,12 +14,12 @@ int main() {
         scanf("%d %s %f", &bonus[i].number, &bonus[i].name, &bonus[i].salary);
     } 
     for (i = 0; i<n ; i++){
-    if (bonus[i].salary < 50000){
-        bonus[i].salary = bonus[i].salary * 0.1 ; 
-    }
-    else {
-        bonus[i].salary = bonus[i].salary * 0.05 ; 
-    }
+        if (bonus[i].salary < 50000){
+            bonus[i].salary = bonus[i].salary * 0.1 ; 
+        }
+        else {
+            bonus[i].salary = bonus[i].salary * 0.05 ; 
+        }
     }
 
     for (i = 0; i < n; i++) {
